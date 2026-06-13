4f2b928a3588d707c7bcd8838c9f2d1b158239ab
ZMYX007 <898198685@qq.com>
2026-06-14 00:36:13 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+DATA_ROOT = "./data"
+BATCH_SIZE = 64
+EPOCH_NUM = 3
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+LR_LIST = [1e-2, 1e-3, 1e-4]
+SAVE_MODEL_DIR = "./saved_models"
+TB_LOG_ROOT = "./logs/compare_lr"
